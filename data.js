3d354f060362bd1adea66fd55ece988e9144aae8
bayudiarta <bayudiarta230@gmail.com>
2022-02-18 16:21:14 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+let todo = [
+    {
+        id: 1,
+        task: 'Learn JavaScript'
+    },
+    {
+        id: 2,
+        task: 'Learn React'
+    },
+    {
+        id: 3,
+        task: 'Learn Angular'
+    },
+]
+
+module.exports = function () {
+    return {
+        todo:todo,
+    };
+};
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <table class="table table-bordered mt-5">
+          <thead>
+            <tr>
+              <th>Todo</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item,i) in todoList" :key="i">
+              <td class="align-middle w-75">
+                {{ item.task }}
+              </td>
+              <td class="align-middle text-center w-75">
+                <button class="btn btn-info btn-sm mx-1" 
+                    v-on:click="handleEdit(item.id)">Edit
+                </button>
+                <button class="btn btn-danger btn-sm mx-1" 
+                    @click="handleDelete(item.id)">Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+                <td class="align-middle text-center w-75">
+                  <div class="form-group">
+                    <label for="">{{
+                        editMode ? 'Edit' : 'Add'
+                      }}</label>
+                      <input type="text" class="form-control" 
+                      v-model="todoItem.task"/>
+                  </div>
+                </td>
+                <td class="align-middle text-center w-75">
+                  <button class="btn btn-primary btn-sm mx-1" 
+                    v-on:click="handleTodoItem">
+                      {{
+                        editMode ? 'Edit' : 'Add'
+                      }}
+                  </button>
+                  <button 
+                    v-if="editMode"
+                    class="btn btn-danger btn-sm mx-1" 
+                    v-on:click="handleCancel">Cancel
+                    </button>
+                </td>
+              </tfoot> 
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Axios from "axios";
+let baseUrl = "http://localhost:3500/todo"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      todoList : [],
+      todoItem: {},
+      editMode: false
+    }
+  },
+  methods : {
+    handleEdit(id) {
+      this.editMode = true;
+      this.todoItem = this.todoList.find(item => item.id === id);
+    },
+    handleCancel() {
+      this.editMode = false;
+      this.todoItem = "";
+    },
+    async handleTodoItem() {
+      const id = this.todoItem.id;
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        if(this.editMode) {
+          await Axios.put(`$(baseUrl)/${id}`, this.todoItem);
+          this.editMode = false;
+          this.todoItem = "";
+        } else {
+          await Axios.post(baseUrl, this.todoItem);
+          this.todoItem.task = "";
+        }
+        Axios.get(baseUrl).then(
+            (response) => (this.todoList = response.data)
+        );
+    },
+    async handleDelete(id) {
+        await Axios.delete(`http://localhost:3500/todo/${id}`);
+        Axios.get(baseUrl).then(
+            (response) => (this.todoList = response.data)
+        );
+    },
+  },
+  created() {
+    Axios.get(baseUrl).then((response) => (this.todoList = response.data));
+  },
+};
+</script>
